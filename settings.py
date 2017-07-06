837f78488549edf630bcb0086aace2caaed57b9c
fabioost <fabioost@bol.com.br>
2017-07-05 21:16:26 -0300

    Adicionando primeiros arquivos

---
+++ settings.py
@@ -0,0 +1,9 @@
+class Settings():
+    """A class to stor all settings of Alien Invasion"""
+    def __init__(self):
+        """Initialize the game setings"""
+		#Screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230,230,230)
+		
