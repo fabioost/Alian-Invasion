fb8b98c8463d7cc435655ae1684be90bb7a3f0c2
fabioost <fabioost@bol.com.br>
2017-07-06 20:59:41 -0300

    Adicionando funcionalidades

--- alien_invasion.py
+++ alien_invasion.py
@@ -14,18 +14,13 @@ def run_game():
     pygame.display.set_caption("Alien Invasion")
     
     # Make a ship.
-    ship = Ship(screen)
+    ship = Ship(ai_settings, screen)
     
     
     # Start the main loop for the game.
     while True:
-        gf.check_events()       
-        # Redraw the screen during each pass through the loop.
-        screen.fill(ai_settings.bg_color)
-        
-        ship.blitme()  
-               
-        # Make the most recently drawn screen visible.
-        pygame.display.flip()
+        gf.check_events(ship)
+        ship.update()       
+        gf.update_screen(ai_settings, screen, ship)
     
 run_game()
